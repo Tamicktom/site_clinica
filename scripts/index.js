0316c00e18777cb93460f68ed82f515102be0528
Tamicktom <60244227+Tamicktom@users.noreply.github.com>
2021-11-27 14:41:58 -0300

    atualizações e adicionamento de novas páginas

--- scripts/index.js
+++ scripts/index.js
@@ -12,3 +12,45 @@ $(document).ready(function() {
         }
     });
 });
+
+//* Código para verificar o tamanho da window
+
+window.addEventListener('resize', windowSizeUpdate);
+
+
+function windowSizeUpdate() {
+    var maxWindowWidth = window.innerWidth;
+    var maxWindowHeight = window.innerHeight;
+    if (window.innerWidth > 992) {
+        changeCarouselImg();
+    }
+    console.log('window size - height[' + maxWindowHeight + 'px]' + ' | width[' + maxWindowWidth + 'px]');
+}
+
+$(document).ready(function() {
+    windowSizeUpdate();
+})
+
+function changeCarouselImg() {
+    if (window.innerWidth > 992) {
+        carouselChangeImgToBigger();
+    } else {
+        changeCarouselImgToSmaller();
+    }
+}
+
+function carouselChangeImgToBigger() {
+    for (var i = 1; i <= 4; i++) {
+        var carouselImg = document.querySelector("#carouselImg" + i);
+        carouselImg.src = 'sources/images/BigBanner_img_' + i + '.png';
+        console.log('funfos');
+    }
+}
+
+function changeCarouselImgToSmaller() {
+    for (var i = 1; i <= 4; i++) {
+        var carouselImg = document.querySelector("#carouselImg" + i);
+        carouselImg.src = 'sources/images/SmallBanner_img_' + i + '.png';
+        console.log('funfos');
+    }
+}
