368a95b1c5042501ce8d196294ef933d51668206
Tamicktom <60244227+Tamicktom@users.noreply.github.com>
2021-11-27 21:27:26 -0300

    versão 1.0

---
+++ creditos.md
@@ -0,0 +1,6 @@
+# Obrigado por extender o prazo :3
+
+Todas imagens utilizadas no site foram todas pegas no freepik.com, com exceção da logo, que foi eu quem fiz.
+As fontes foram pegas no dafont.com.
+
+# aluno: Henrique Ângelo Vieira Fonseca
--- scripts/index.js
+++ scripts/index.js
@@ -19,12 +19,9 @@ window.addEventListener('resize', windowSizeUpdate);
 
 
 function windowSizeUpdate() {
-    var maxWindowWidth = window.innerWidth;
-    var maxWindowHeight = window.innerHeight;
     if (window.innerWidth > 992) {
         changeCarouselImg();
     }
-    console.log('window size - height[' + maxWindowHeight + 'px]' + ' | width[' + maxWindowWidth + 'px]');
 }
 
 $(document).ready(function() {
@@ -53,4 +50,40 @@ function changeCarouselImgToSmaller() {
         carouselImg.src = 'sources/images/SmallBanner_img_' + i + '.png';
         console.log('funfos');
     }
+}
+
+
+//* Mascara para a caixa de cpf
+function mascaraUser(o, f) {
+    v_obj = o;
+    v_fun = f;
+    setTimeout('execmascara()', 1);
+}
+
+function execmascara() {
+    v_obj.value = v_fun(v_obj.value);
+}
+
+function cpfCnpj(v) {
+    //Remove tudo o que não é dígito
+    v = v.replace(/\D/g, "");
+    if (v.length < 14) {
+        //Coloca um ponto entre o terceiro e o quarto dígitos
+        v = v.replace(/(\d{3})(\d)/, "$1.$2")
+            //Coloca um ponto entre o terceiro e o quarto dígitos
+            //de novo (para o segundo bloco de números)
+        v = v.replace(/(\d{3})(\d)/, "$1.$2")
+            //Coloca um hífen entre o terceiro e o quarto dígitos
+        v = v.replace(/(\d{3})(\d{1,2})$/, "$1-$2")
+    } else {
+        //Coloca ponto entre o segundo e o terceiro dígitos
+        v = v.replace(/^(\d{2})(\d)/, "$1.$2");
+        //Coloca ponto entre o quinto e o sexto dígitos
+        v = v.replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3");
+        //Coloca uma barra entre o oitavo e o nono dígitos
+        v = v.replace(/\.(\d{3})(\d)/, ".$1/$2");
+        //Coloca um hífen depois do bloco de quatro dígitos
+        v = v.replace(/(\d{4})(\d)/, "$1-$2");
+    }
+    return v;
 }
